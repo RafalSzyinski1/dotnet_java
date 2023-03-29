a978bfcb8a3e305b33ac95f5fcbb1cc427dd738a
Rafał Szyiński <77827305+RafalSzyinski1@users.noreply.github.com>
2023-03-29 14:08:09 +0200

    form

--- KnapsackWindow/Form1.Designer.cs
+++ KnapsackWindow/Form1.Designer.cs
@@ -28,146 +28,186 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.num_of_items_label = new System.Windows.Forms.Label();
+            this.seed_label = new System.Windows.Forms.Label();
+            this.volume_label = new System.Windows.Forms.Label();
+            this.output_box = new System.Windows.Forms.RichTextBox();
             this.label4 = new System.Windows.Forms.Label();
-            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.knapsack_box = new System.Windows.Forms.RichTextBox();
             this.label5 = new System.Windows.Forms.Label();
+            this.gen_button = new System.Windows.Forms.Button();
+            this.num_of_items_numeric = new System.Windows.Forms.NumericUpDown();
+            this.seed_numeric = new System.Windows.Forms.NumericUpDown();
+            this.volume_numeric = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.num_of_items_numeric)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.seed_numeric)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.volume_numeric)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // num_of_items_label
             // 
-            this.button1.Location = new System.Drawing.Point(308, 54);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.num_of_items_label.AutoSize = true;
+            this.num_of_items_label.Location = new System.Drawing.Point(45, 30);
+            this.num_of_items_label.Name = "num_of_items_label";
+            this.num_of_items_label.Size = new System.Drawing.Size(90, 16);
+            this.num_of_items_label.TabIndex = 4;
+            this.num_of_items_label.Text = "Num of items: ";
             // 
-            // textBox1
+            // seed_label
             // 
-            this.textBox1.Location = new System.Drawing.Point(183, 26);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 22);
-            this.textBox1.TabIndex = 1;
+            this.seed_label.AutoSize = true;
+            this.seed_label.Location = new System.Drawing.Point(45, 58);
+            this.seed_label.Name = "seed_label";
+            this.seed_label.Size = new System.Drawing.Size(46, 16);
+            this.seed_label.TabIndex = 5;
+            this.seed_label.Text = "Seed: ";
             // 
-            // textBox2
+            // volume_label
             // 
-            this.textBox2.Location = new System.Drawing.Point(183, 55);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 22);
-            this.textBox2.TabIndex = 2;
+            this.volume_label.AutoSize = true;
+            this.volume_label.Location = new System.Drawing.Point(45, 90);
+            this.volume_label.Name = "volume_label";
+            this.volume_label.Size = new System.Drawing.Size(116, 16);
+            this.volume_label.TabIndex = 6;
+            this.volume_label.Text = "Knapsnck volume:";
             // 
-            // textBox3
+            // output_box
             // 
-            this.textBox3.Location = new System.Drawing.Point(183, 83);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 22);
-            this.textBox3.TabIndex = 3;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(45, 30);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 16);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "label1";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(45, 58);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(44, 16);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "label2";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(45, 90);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(44, 16);
-            this.label3.TabIndex = 6;
-            this.label3.Text = "label3";
-            // 
-            // richTextBox1
-            // 
-            this.richTextBox1.Location = new System.Drawing.Point(411, 49);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(377, 389);
-            this.richTextBox1.TabIndex = 7;
-            this.richTextBox1.Text = "";
+            this.output_box.Location = new System.Drawing.Point(411, 49);
+            this.output_box.Name = "output_box";
+            this.output_box.ReadOnly = true;
+            this.output_box.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedVertical;
+            this.output_box.Size = new System.Drawing.Size(377, 389);
+            this.output_box.TabIndex = 7;
+            this.output_box.Text = "";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Location = new System.Drawing.Point(408, 30);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(44, 16);
+            this.label4.Size = new System.Drawing.Size(43, 16);
             this.label4.TabIndex = 8;
-            this.label4.Text = "label4";
+            this.label4.Text = "output";
             // 
-            // richTextBox2
+            // knapsack_box
             // 
-            this.richTextBox2.Location = new System.Drawing.Point(48, 178);
-            this.richTextBox2.Name = "richTextBox2";
-            this.richTextBox2.Size = new System.Drawing.Size(313, 260);
-            this.richTextBox2.TabIndex = 9;
-            this.richTextBox2.Text = "";
+            this.knapsack_box.Location = new System.Drawing.Point(48, 178);
+            this.knapsack_box.Name = "knapsack_box";
+            this.knapsack_box.ReadOnly = true;
+            this.knapsack_box.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedVertical;
+            this.knapsack_box.Size = new System.Drawing.Size(313, 260);
+            this.knapsack_box.TabIndex = 9;
+            this.knapsack_box.Text = "";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Location = new System.Drawing.Point(48, 156);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(44, 16);
+            this.label5.Size = new System.Drawing.Size(66, 16);
             this.label5.TabIndex = 10;
-            this.label5.Text = "label5";
+            this.label5.Text = "knapsack";
+            // 
+            // gen_button
+            // 
+            this.gen_button.Location = new System.Drawing.Point(309, 49);
+            this.gen_button.Name = "gen_button";
+            this.gen_button.Size = new System.Drawing.Size(75, 39);
+            this.gen_button.TabIndex = 0;
+            this.gen_button.Text = "generate";
+            this.gen_button.UseVisualStyleBackColor = true;
+            this.gen_button.Click += new System.EventHandler(this.gen_button_Click);
+            // 
+            // num_of_items_numeric
+            // 
+            this.num_of_items_numeric.Location = new System.Drawing.Point(173, 24);
+            this.num_of_items_numeric.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.num_of_items_numeric.Name = "num_of_items_numeric";
+            this.num_of_items_numeric.Size = new System.Drawing.Size(120, 22);
+            this.num_of_items_numeric.TabIndex = 11;
+            this.num_of_items_numeric.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // seed_numeric
+            // 
+            this.seed_numeric.Location = new System.Drawing.Point(173, 56);
+            this.seed_numeric.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.seed_numeric.Name = "seed_numeric";
+            this.seed_numeric.Size = new System.Drawing.Size(120, 22);
+            this.seed_numeric.TabIndex = 12;
+            this.seed_numeric.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // volume_numeric
+            // 
+            this.volume_numeric.Location = new System.Drawing.Point(173, 90);
+            this.volume_numeric.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.volume_numeric.Name = "volume_numeric";
+            this.volume_numeric.Size = new System.Drawing.Size(120, 22);
+            this.volume_numeric.TabIndex = 13;
+            this.volume_numeric.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.volume_numeric);
+            this.Controls.Add(this.seed_numeric);
+            this.Controls.Add(this.num_of_items_numeric);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.knapsack_box);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.richTextBox1);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.output_box);
+            this.Controls.Add(this.volume_label);
+            this.Controls.Add(this.seed_label);
+            this.Controls.Add(this.num_of_items_label);
+            this.Controls.Add(this.gen_button);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.num_of_items_numeric)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.seed_numeric)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.volume_numeric)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label num_of_items_label;
+        private System.Windows.Forms.Label seed_label;
+        private System.Windows.Forms.Label volume_label;
+        private System.Windows.Forms.RichTextBox output_box;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox knapsack_box;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button gen_button;
+        private System.Windows.Forms.NumericUpDown num_of_items_numeric;
+        private System.Windows.Forms.NumericUpDown seed_numeric;
+        private System.Windows.Forms.NumericUpDown volume_numeric;
     }
 }
 
--- KnapsackWindow/Form1.cs
+++ KnapsackWindow/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using lab1;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,16 @@ namespace KnapsackWindow
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void gen_button_Click(object sender, EventArgs e)
+        {
+            int num_of_items = (int)num_of_items_numeric.Value;
+            int seed = (int)seed_numeric.Value;
+            int knapsack_volume = (int)volume_numeric.Value;
+            Knapsack knapsack = new Knapsack(num_of_items, seed);
+            knapsack_box.Text = knapsack.ToString();
+            output_box.Text = knapsack.run(knapsack_volume).ToString();
         }
     }
 }
