164f3a0dfc1f75bd03de9e128c3af8a30b220270
Rafał Szyiński <77827305+RafalSzyinski1@users.noreply.github.com>
2023-04-19 15:16:33 +0200

    DFT

--- lab3/Program.cs
+++ lab3/Program.cs
@@ -11,6 +11,39 @@ namespace lab3
     {
         static void Main(string[] args)
         {
+            List<double> signalX = new List<double>();
+            List<double> signalY = new List<double>();
+            for (double i = 0.0; i < 2 * Math.PI * 10; i += 0.5)
+            {
+                signalX.Add(i);
+                signalY.Add(Math.Sin(i));
+            }
+
+            for (int i = 0; i < signalX.Count; ++i)
+            {
+                Console.WriteLine(signalX[i]);
+            }
+            Console.WriteLine();
+            for (int i = 0; i < signalY.Count; ++i)
+            {
+                Console.WriteLine(signalY[i]);
+            }
+            Console.WriteLine();
+
+            DFT dft = new DFT();
+            dft.Run(signalX, signalY);
+
+            for (int i = 0; i < dft.Re.Count; ++i)
+            {
+                Console.WriteLine(dft.Amp[i]);
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < dft.Re.Count; ++i)
+            {
+                Console.WriteLine(dft.Freq[i]);
+            }
+            Console.ReadLine();
         }
     }
 }
