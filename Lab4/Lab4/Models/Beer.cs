797379b9a6bbe4d7a5ed8eded48fd3e985a97048
mardor2001 <259295@student.pwr.edu.pl>
2023-05-16 13:27:32 +0200

    z zajęć

---
+++ Lab4/Lab4/Models/Beer.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab4.Models
+{
+    public class Beer
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public float Price { get; set; }
+        public string Type { get; set; }
+    }
+}
