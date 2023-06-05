042048c447d4cb9bb400b73741848bf0596d9384
Rafał Szyiński <77827305+RafalSzyinski1@users.noreply.github.com>
2023-06-05 14:02:02 +0200

    init | test program

---
+++ src/Core.java
@@ -0,0 +1,77 @@
+
+/**
+ * ---------------- Java 2D Graphics ---------------- 
+ * Java 2D Bouncing Ball
+ * This class contains motion logic and graphics rendering method.
+ * Developed with Eclipse IDE
+ * @author DannyWarp
+ * 
+ */
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JComponent;
+import javax.swing.Timer;
+
+public class Core extends JComponent {
+
+	private static final long serialVersionUID = 1L;
+	// Global variables.
+	int x, y, bounds = 80;
+	boolean move_up, move_left;
+
+	public Core() {
+		// The timer is used to repaint the component.
+		Timer timer = new Timer(10, new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// horizontal motion logic.
+				if (x > getWidth() - bounds) {
+					move_left = true;
+				}
+				if (x < 0) {
+					move_left = false;
+				}
+				// Performing horizontal motion.
+				if (move_left) {
+					x -= 1;
+				} else {
+					x += 1;
+				}
+				// vertical motion logic.
+				if (y > getHeight() - bounds) {
+					move_up = true;
+				}
+				if (y < 0) {
+					move_up = false;
+				}
+				// Performing vertical motion.
+				if (move_up) {
+					y -= 1;
+				} else {
+					y += 1;
+				}
+				repaint(); // Repaint JComponent.
+			}
+		});
+		timer.start(); // I think you have brains.
+
+	}
+
+	public void paintComponent(Graphics g) { // This method is called using EDT, it does not need to be called.
+		Graphics2D g2d = (Graphics2D) g; // Conversion from Graphics to Graphics2D.
+		super.paintComponent(g2d); // I don't know what that.
+		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON); // Better resolution
+																									// of the shapes.
+		g2d.setColor(Color.RED); // Setting color of all shapes below.
+		g2d.fillOval(x, y, bounds, bounds); // Draw the circle.
+		g2d.dispose(); // Memory optimization. Helps a lot if method has create new shape objects.
+		Toolkit.getDefaultToolkit().sync(); // Rendering are OS dependent. This line makes animation smoother on Linux.
+	}
+
+}
---
+++ src/Interface.java
@@ -0,0 +1,37 @@
+
+/**
+ * ---------------- Java 2D Graphics ---------------- 
+ * Java 2D Bouncing Ball
+ * This class creates the window.
+ * Developed with Eclipse IDE
+ * @author DannyWarp
+ * 
+ */
+
+import java.awt.BorderLayout;
+import java.awt.Image;
+import javax.swing.JFrame;
+
+public class Interface extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+
+	public Interface() { // Constructor.
+
+		setSize(840, 500); // Setting window size.
+		setLocationRelativeTo(null); // Centering the window.
+		setDefaultCloseOperation(EXIT_ON_CLOSE); // Shut down the program.
+		setLayout(new BorderLayout()); // Setting the border layout.
+		setTitle("Bouncing Ball Animation"); // Setting the program title.
+
+		// image (only if this JAR
+		// build).
+		add(new Core()); // Adding a class to JFrame (the class must be of type Component ("extends
+							// JPanel, JComponent, etc.).
+		setVisible(true); // Setting visibility of the program.
+	}
+
+	public static void main(String[] args) {
+		new Interface(); // Run the program
+	}
+}
