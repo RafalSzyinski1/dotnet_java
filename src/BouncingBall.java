d333d178f3403197c68a03c01278c484e516fc07
mardor2001 <259295@student.pwr.edu.pl>
2023-06-10 09:44:56 +0200

    lalal

---
+++ src/BouncingBall.java
@@ -0,0 +1,88 @@
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class BouncingBall extends Ball {
+
+    public BouncingBall(Box box) {
+        Random rand = new Random();
+
+        // Testing values
+        setRadius(rand.nextInt(30));
+        setSpeed(rand.nextInt(300), rand.nextInt(300));
+        setColor(new Color(rand.nextInt(255), rand.nextInt(255), rand.nextInt(255)));
+    }
+
+    public void checkWall(Box box) {
+        lock.lock();
+        double act_pos[] = getPosition();
+        double act_speed[] = getSpeed();
+        lock.unlock();
+        double ballMinX = box.minX + radius;
+        double ballMinY = box.minY + radius;
+        double ballMaxX = box.maxX - radius;
+        double ballMaxY = box.maxY - radius;
+
+        if (act_pos[0] < ballMinX) {
+            lock.lock();
+            setSpeed(-act_speed[0], act_speed[1]);
+            setPosition(ballMinX, act_pos[1]);
+            lock.unlock();
+        } else if (act_pos[0] > ballMaxX) {
+            lock.lock();
+            setSpeed(-act_speed[0], act_speed[1]);
+            setPosition(ballMaxX, act_pos[1]);
+            lock.unlock();
+        }
+
+        if (act_pos[1] < ballMinY) {
+            lock.lock();
+            setSpeed(act_speed[0], -act_speed[1]);
+            setPosition(act_pos[0], act_pos[1]);
+            lock.unlock();
+        } else if (act_pos[1] > ballMaxY) {
+            lock.lock();
+            setSpeed(act_speed[0], -act_speed[1]);
+            setPosition(act_pos[0], act_pos[1]);
+            lock.unlock();
+        }
+/*
+        lock.lock();
+        act_pos[0] += act_speed[0];
+        act_pos[1] += act_speed[1];
+        lock.unlock();
+    */  }
+
+    public void checkOthers(ArrayList<BouncingBall> balls,double dt) {
+        lock.lock();
+        double my_pos[] = getPosition();
+        double my_speed[] = getSpeed();
+        double my_radius = radius;
+        lock.unlock();
+
+        for (Ball ball : balls) {
+            lock.lock();
+            double his_pos[] = ball.getPosition();
+            double his_speed[] = ball.getSpeed();
+            double his_radius = ball.radius;
+            lock.unlock();
+
+            if (my_pos == his_pos) {
+                continue;
+            }
+
+            double distance = Math.sqrt(Math.pow(my_pos[0] - his_pos[0], 2) + Math.pow(my_pos[1] - his_pos[1], 2));
+            if(distance < (my_radius + his_radius)*0.9){
+                ball.setPosition(his_pos[0]+his_speed[0]*dt, his_pos[1]+his_speed[1]*dt);
+            }
+            else if (distance <= my_radius + his_radius) {
+                lock.lock();
+                ball.setSpeed(-his_speed[0], -his_speed[1]);
+                //ball.update(my_radius/60.0);
+                lock.unlock();
+                
+            }
+        }
+    }
+
+}
---
+++ src/World.java
@@ -0,0 +1,145 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class World extends JPanel {
+    private static final int UPDATE_RATE = 100;
+    protected ReentrantLock lock = new ReentrantLock();
+    private Box box;
+    private ArrayList<BouncingBall> balls;
+
+    private DrawCanvas canvas;
+
+    public World(int width, int height) {
+        balls = new ArrayList<BouncingBall>();
+
+        Random rand = new Random();
+
+        box = new Box(0, 0, width, height);
+
+        for (int i = 0; i < 20; ++i) {
+            BouncingBall ball = new BouncingBall(box);
+
+            ball.setPosition(rand.nextInt(width - 60) + 30, rand.nextInt(height - 60) + 30);
+            balls.add(ball);
+
+        }
+
+        canvas = new DrawCanvas();
+        this.setLayout(new BorderLayout());
+        this.add(canvas, BorderLayout.CENTER);
+
+        this.addComponentListener(new ComponentAdapter() {
+            @Override
+            public void componentResized(ComponentEvent e) {
+                Component c = (Component) e.getSource();
+                Dimension dim = c.getSize();
+                box.set(0, 0, dim.width, dim.height);
+            }
+        });
+
+        gameRun();
+    }
+
+    public void gameRun() {
+
+        Thread gameThread = new Thread() {
+            public void run() {
+                double dt = 1.0/UPDATE_RATE;
+                Thread[] Ball_life = new Thread[balls.size()];
+                int index = 0;
+                for(BouncingBall ball: balls){
+                    Ball_life[index] = new Thread(()->update(ball,dt));
+                    Ball_life[index].start();
+                    index++;
+                }
+                while (true) {
+                    long beginTimeMillis, timeTakenMillis, timeLeftMillis;
+                    beginTimeMillis = System.currentTimeMillis();
+
+                    //gameUpdate(1.0 / UPDATE_RATE);
+                    repaint();
+
+                    timeTakenMillis = System.currentTimeMillis() - beginTimeMillis;
+                    timeLeftMillis = 1000L / UPDATE_RATE - timeTakenMillis;
+                    if (timeLeftMillis < 5)
+                        timeLeftMillis = 5;
+                    try {
+                        Thread.sleep(timeLeftMillis);
+                    } catch (InterruptedException ex) {
+                    }
+                }
+            }
+        };
+        gameThread.start();
+    }
+
+    public void update(BouncingBall ball, double dt){
+        while(true){
+            long beginTimeMillis, timeTakenMillis, timeLeftMillis;
+                    beginTimeMillis = System.currentTimeMillis();
+
+                    lock.lock();
+                    ball.checkOthers(balls,dt);
+                    lock.unlock();
+                    lock.lock();
+                    ball.checkWall(box);
+                    lock.unlock();
+                    lock.lock();
+                    ball.update(dt);
+                    lock.unlock();
+
+                    timeTakenMillis = System.currentTimeMillis() - beginTimeMillis;
+                    timeLeftMillis = 1000L / UPDATE_RATE - timeTakenMillis;
+                    if (timeLeftMillis < 5)
+                        timeLeftMillis = 5;
+                    try {
+                        Thread.sleep(timeLeftMillis);
+                    } catch (InterruptedException ex) {
+                    }
+        }
+    }
+
+    public void gameUpdate(double dt) {
+        for (Ball ball : balls)
+            ball.update(dt);
+        int balls_size = balls.size();
+        Thread[] CheckWall = new Thread[balls_size];
+        Thread[] CheckOthers = new Thread[balls_size];
+
+        int index = 0;
+        for (BouncingBall ball : balls) {
+            CheckWall[index] = new Thread(() -> ball.checkWall(box));
+            CheckWall[index].start();
+            CheckOthers[index] = new Thread(() -> ball.checkOthers(balls,dt));
+            CheckOthers[index].start();
+            index = index + 1;
+        }
+        for (int i = 0; i < index; i++) {
+            try {
+                CheckWall[i].join();
+                CheckOthers[i].join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    class DrawCanvas extends JPanel {
+        @Override
+        public void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            box.draw(g);
+            for (Ball ball : balls)
+                ball.draw(g);
+        }
+
+        @Override
+        public Dimension getPreferredSize() {
+            return (new Dimension(box.maxX - box.minX - 1, box.maxY - box.minY - 1));
+        }
+    }
+}
